d2a702eaf438771f338b8548a323ef5059c90446
Your Name <selmanovic.edin1@gmail.com>
2023-01-03 12:53:20 +0100

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let helloWorld = "Hello World"
+console.log(helloWorld);
+
+const pi = 3.14
+
+const burjdubai = "825 m"
+console.log(burjdubai);
+
+let fullName = "Jan Schmidt"
+console.log(fullName);
+
+const eifelTower = "324m"
+console.log(eifelTower);
+
+let camelCasse = "camelcase"
+console.log(camelCasse);
+
+let kursStatus = true
+console.log(kursStatus);
